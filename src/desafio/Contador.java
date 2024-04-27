7d897434ed22ad04a0885e0168fdbab75e6884e2
pedro bovo <pedrobovo123@gmail.com>
2024-04-27 11:28:12 -0300

    Resolução do desafio

---
+++ src/desafio/Contador.java
@@ -0,0 +1,34 @@
+package desafio;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = s.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = s.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }else{
+            int contagem = parametroDois - parametroUm;
+            for (int i = 1; i <= contagem; i++) {
+                System.out.println("Imprimindo o número " + i);
+            }
+        }
+
+
+    }
+}
